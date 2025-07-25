84b4d74704b672ca0cf4381a3cbbfcd700f9a658
iweye <iweye3@gmail.com>
2025-07-25 10:06:51 +0300

    fix: paste mapping in neovim

--- root/root/.config/nvim/lua/mappings.lua
+++ root/root/.config/nvim/lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>")
+map("x", "p", '"_dP', { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
